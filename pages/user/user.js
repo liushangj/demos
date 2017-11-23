7f3e91806a663725097746fb8e368071e2fe8397
liushanj <test@1473595842@qq.com>
2017-11-23 11:15:49 +0800

    添加map

---
+++ pages/user/user.js
@@ -0,0 +1,121 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userUrl:"/static/imgs/girl.png",
+    address: "力宝大厦",
+    name : "...",
+    latitude:0,
+    longitude:0
+  },
+  openMap(){//打开新地图
+    // wx.openLocation({
+    //   latitude: this.data.latitude,
+    //   longitude: this.data.longitude,
+    //   name : this.data.name,
+    //   address:this.data.address
+    // })
+    wx.chooseLocation({//获取当前位置
+      success: function (res) {
+        console.log(res.name, res.address)
+        _this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          address: res.address,
+          name: res.name
+        })
+      },
+    })
+  },
+
+  chooseImg() {//修改头像
+    var _this = this;
+    wx.chooseImage({
+      success: function(res) {
+        console.log(res.tempFilePaths);
+        _this.setData({
+          userUrl:res.tempFilePaths
+        })
+      },
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var _this = this;
+    wx.chooseLocation({//获取当前位置
+      success: function(res) {
+        console.log(res.name,res.address)
+        _this.setData({
+          latitude:res.latitude,
+          longitude: res.longitude ,
+          address:res.address  ,
+          name : res.name       
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    if(res.from ==='button'){
+      //来自页面内的转发按钮
+      console.log(res.target)
+    }
+    return {
+      title : "我的小程序",
+      path : "/pages/user/user",
+      success:function(res){
+        //转发成功
+      }
+    }
+  }
+})
